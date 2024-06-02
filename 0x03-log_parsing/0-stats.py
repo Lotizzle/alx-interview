e6147227557a690230dc3ecb764677cff945a682
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-02 23:57:03 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,71 +1,55 @@
 #!/usr/bin/env python3
 """
-This module contains the print_statistics, parse_line, and main functions.
+Module contains script that reads stdin
+line by line and computes metrics.
 """
+import sys
+import signal
+
+total_size = 0
+status_counts = {}
+valid_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+line_count = 0
 
 
-def print_statistics(total_file_size, code_counts):
-    """Prints current totals of file size and status code counts."""
-    print("File size:", total_file_size)
-    for code in code_counts:
-        if code_counts[code] > 0:
-            print("{}: {}".format(code, code_counts[code]))
+def print_stats():
+    """Print the accumulated metrics."""
+    print(f"File size: {total_size}")
+    for status_code in sorted(status_counts):
+        if status_code in valid_status_codes:
+            print(f"{status_code}: {status_counts[status_code]}")
 
 
-if __name__ == "__main__":
-    from sys import argv, stdin, stderr
-    from collections import OrderedDict
-    from datetime import datetime
+def signal_handler(sig, frame):
+    """Handle the keyboard interruption."""
+    print_stats()
+    sys.exit(0)
 
-    # Initialize variables to store metrics
-    total_file_size = 0
-    status_code_counts = OrderedDict.fromkeys([200, 301, 400, 401, 403,
-                                              404, 405, 500], 0)
-    line_count = 0
 
-    try:
-        for line in stdin:
-            line_count += 1
+# Set the signal handler for keyboard interruption
+signal.signal(signal.SIGINT, signal_handler)
 
-            line_input = line.split('-', 1)
-            if len(line_input) != 2:
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        if len(parts) == 9:
+            file_size = parts[-1]
+            status_code = parts[-2]
+            try:
+                total_size += int(file_size)
+                if (status_code.isdigit() and
+                        int(status_code) in valid_status_codes):
+                    if int(status_code) not in status_counts:
+                        status_counts[int(status_code)] = 1
+                    else:
+                        status_counts[int(status_code)] += 1
+            except ValueError:
                 continue
 
-            # Check timestamp format
-            time_format = line_input[1].split(']')
-            time_code = time_format[0].lstrip(' [')
-            try:
-                datetime.strptime(time_code, '%Y-%m-%d %H:%M:%S.%f')
-            except ValueError:
-                stderr.write("{}: {}: invalid timecode\n".format(
-                             argv[0], line_count))
-                pass
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
 
-            # Check URL
-            url_check = time_format[1].split('"')
-            url_check = url_check[1:]
-            if url_check[0] != 'GET /projects/260 HTTP/1.1':
-                stderr.write("{}: {}: unexpected HTTP request\n".format(
-                             argv[0], line_count))
-
-            # Update metrics
-            metrics = url_check[1].lstrip(' ')
-            metrics = metrics.rstrip('\n')
-            metrics = metrics.split(' ')
-
-            # Check status codes
-            if metrics[0].isdecimal():
-                status_code = int(metrics[0])
-                status_code_counts[status_code] += 1
-
-            # Check file size
-            if metrics[1].isdecimal():
-                total_file_size += int(metrics[1])
-
-            if line_count % 10 == 0:
-                print_statistics(total_file_size, status_code_counts)
-        print_statistics(total_file_size, status_code_counts)
-    except (KeyboardInterrupt):
-        # Handle keyboard interruption (CTRL + C)
-        print_statistics(total_file_size, status_code_counts)
-        raise
+except KeyboardInterrupt:
+    print_stats()
+    sys.exit(0)
