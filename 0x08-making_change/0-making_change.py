a3b4e94771156851008a6a39273c1768c3e2e180
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-06 23:42:43 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module contains Make change function
+"""
+
+
+def makeChange(coins, total):
+    """
+    Finds the minimum amount of coins needed to reach total
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,2 @@
+## 0x08. Making Change
+### Learning about domain of dynamic programming and greedy algorithms
